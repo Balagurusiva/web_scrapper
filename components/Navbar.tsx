297eae2c08cb56152b65bb634d07aa2531a4fed2
Balaguru <balagurusiva2002@gmail.com>
2023-10-28 15:29:01 +0530

    modified:   app/globals.css 	modified:   components/Navbar.tsx 	modified:   public/assets/icons/logo.svg 	modified:   public/assets/icons/search.svg 	modified:   public/assets/icons/user.svg 	renamed:    public/assets/icons/black-heart.svg -> public/assets/icons/white-heart.svg

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,42 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
+const navIcons = [
+    {src:"/assets/icons/search.svg", alt: 'serach'},
+    {src:"/assets/icons/white-heart.svg", alt: 'serach'},
+    {src:"/assets/icons/user.svg", alt: 'serach'},
+]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+     <header className="w-full">
+        <nav className="nav">
+            <Link href='/' className='flex items-center gap-1'>
+               <Image 
+                 src="/assets/icons/logo.svg"
+                 width={27}
+                 height={27}
+                 alt='logo'
+               />
+               <p className="nav-logo">
+                Data<span className='text-primary'>Chisel</span>
+               </p>
+            </Link>
+            <div className="flex items-center gap-5">
+                  {navIcons.map((icon) =>(
+                    <Image 
+                      key={icon.alt}
+                      src={icon.src}
+                      alt={icon.alt}
+                      width= {28}
+                      height={28}
+                      className='object-contain'
+                     />
+                  ))}
+            </div>
+        </nav>
+     </header>
   )
 }
 
