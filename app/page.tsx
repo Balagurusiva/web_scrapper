32b04ba5f3ca22002ffa4626cc51d6414a3d2650
Balaguru <balagurusiva2002@gmail.com>
2023-10-30 14:39:43 +0530

    modified:   app/globals.css 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
  
  const Home = () => {
    return (
+    <> 
      <section className="px-6 md:px-20 py-24 border-2 border-cyan-500">
         <div className="flex max-xl:flex-col gap-16">
             <div className="flex flex-col justify-center">
@@ -19,9 +20,28 @@ import React from 'react'
                     unleassh the power of 
                     <span className="text-cyan-500"> PriceWise</span>
                 </h1>
+
+                <p className="mt-6">
+                    Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+                </p>
+                Serchbar           
             </div>
+            HeroCarousel
         </div>
      </section>
+
+     <section className="trending-section">
+        <h2 className="section-text ">Trending</h2>
+
+        <div className="flex flex-wrap gap-8 gap-y-16">
+            {
+                ["apple", 'bala', 'asdf'].map((item) =>(
+                    <div>{item}</div>
+                ))
+            }
+        </div>
+     </section>
+     </>
    )
  }
  
