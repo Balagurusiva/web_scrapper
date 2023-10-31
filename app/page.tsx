815f8ffebfa5624ae82edb75f83e260d933b2302
Balaguru <balagurusiva2002@gmail.com>
2023-10-31 11:39:31 +0530

    modified:   app/globals.css 	modified:   app/page.tsx 	new file:   components/HeroCarousel.tsx 	new file:   components/Searchbar.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
  import Image from 'next/image'
 import React from 'react'
+import Searchbar from '../components/Searchbar'
+import HeroCarousel from '../components/HeroCarousel'
  
  const Home = () => {
    return (
     <> 
-     <section className="px-6 md:px-20 py-24 border-2 border-cyan-500">
+     <section className="px-6 md:px-20 py-14 ">
         <div className="flex max-xl:flex-col gap-16">
             <div className="flex flex-col justify-center">
                 <p className="small-text">
@@ -24,9 +26,9 @@ import React from 'react'
                 <p className="mt-6">
                     Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
                 </p>
-                Serchbar           
+                <Searchbar />           
             </div>
-            HeroCarousel
+            <HeroCarousel />
         </div>
      </section>
 
---
+++ components/HeroCarousel.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css" ;
+import { Carousel } from 'react-responsive-carousel' ;
+import Image from 'next/image';
+
+const HeroCarousel = () => {
+    const heroImg = [
+        {imgUrl : '/assets/images/hero-1.svg', alt : 'smartwatch'},
+        {imgUrl : '/assets/images/hero-2.svg', alt : 'bag'},
+        {imgUrl : '/assets/images/hero-3.svg', alt : 'lamp'},
+        {imgUrl : '/assets/images/hero-4.svg', alt : 'air fresher'},
+        {imgUrl : '/assets/images/hero-5.svg', alt : 'chair'},
+    ]
+  return (
+    <div className='hero-carousel flex justify-center'>
+        <Carousel
+           showThumbs={false}
+           autoPlay
+           showIndicators={false}
+           infiniteLoop
+           interval={2000}
+           showArrows ={ false}
+           showStatus = {false}
+        >
+             {
+                heroImg.map((image) => (
+                    <Image 
+                        src={image.imgUrl} 
+                        alt={image.alt}
+                        width={304}
+                        height={304}
+                        className='object-contain'
+                        key={image.alt}
+                        />
+                ))
+             }
+        </Carousel>
+    </div>
+  )
+}
+
+export default HeroCarousel
