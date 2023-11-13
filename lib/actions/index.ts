2ed233dcbd7f5e9f55725ade6f6a53e169eb4362
Balaguru <balagurusiva2002@gmail.com>
2023-11-13 09:26:47 +0530

    modified:   lib/actions/index.ts 	new file:   lib/models/product.model.ts 	modified:   package-lock.json 	modified:   package.json

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,8 +1,11 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 import { connect } from "http2";
 import { scrapAmazonProduct } from "../scraper";
 import { connectToDB } from "../scraper/mogoose";
+import Product from "../models/product.model";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../scraper/utils";
 
 export async function scrapAndStoreProduct(productUrl : string){
     if(!productUrl) return;
@@ -11,9 +14,34 @@ export async function scrapAndStoreProduct(productUrl : string){
         connectToDB()
 
         const scrapedProduct = await scrapAmazonProduct(productUrl); 
-
         if(!scrapedProduct) return;
 
+        let product = scrapedProduct
+
+        const existingproduct = await Product.findOne({url : scrapedProduct.url})
+
+        if(existingproduct){
+            const updatedpriceHistory : any= [
+                ...existingproduct.priceHistory,
+                {price:scrapedProduct.currentPrice}
+            ]
+
+            product = {
+                ...scrapedProduct,
+                priceHistory: updatedpriceHistory,
+                lowestPrice:getLowestPrice(updatedpriceHistory),
+                higestPrice:getHighestPrice(updatedpriceHistory),
+                averagePrice:getAveragePrice(updatedpriceHistory),
+            }
+        }
+
+        const newProduct = await Product.findOneAndUpdate({
+            url:scrapedProduct.url},
+            product,
+            {upsert:true, new:true}
+        );
+
+        revalidatePath(`/products/$(newProduct._id)`)
     } catch (error: any) {
         throw new Error(`failed to create/update : ${error.message}`)
     }
