24c90ac27fe23bff0d40281917ef5077ac3d6677
Balaguru S <balagurusiva2002@gmail.com>
2023-11-19 09:42:00 +0530

    use server change

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -8,9 +8,9 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
  
  
 
-// export const maxDuration = 10; // This function can run for a maximum of 300 seconds
-// export const dynamic = "force-dynamic";
-// export const revalidate = 0;
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
